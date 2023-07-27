716954c19d45a2161466635aaebf79c9891692c5
Gabriel <gmgmdaos@gmail.com>
2023-07-26 22:35:31 -0500

    Se agrega funcionalidad a los botones

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 // Import our custom CSS
-import './src/sass/main.scss'
-import * as bootstrap from 'bootstrap'
+import './src/sass/main.scss';
+import * as bootstrap from 'bootstrap';
+
+const payContent = document.querySelector('.card__pay');
+const receiptCotent = document.querySelector('.card__receipt');
+const errorContet = document.querySelector('.card__error');
+
+const btnPay = document.querySelector('.btn__pay');
+const btnReceipt = document.querySelector('.btn__receipt');
+
+//* cambiar entre true y false para ver la tarjeta de error o de recibo
+let isError = false;
+
+btnPay.addEventListener('click', (e) => {
+  payContent.classList.add('hidden');
+  if (isError) {
+    errorContet.classList.remove('hidden');
+  } else if (!isError) {
+    receiptCotent.classList.remove('hidden');
+  }
+});
+
+btnReceipt.addEventListener('click', (e) => {
+  payContent.classList.remove('hidden');
+  errorContet.classList.add('hidden');
+  receiptCotent.classList.add('hidden');
+});
